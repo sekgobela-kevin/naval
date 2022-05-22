670d14a949f1bef3d076943c7ba85d6ee17732a8
Sekgobela-Kevin <kevinnoko23@gmail.com>
2022-05-22 12:00:49 +0200

    Added new test methods

--- tests/unit/crawler/test_sources.py
+++ tests/unit/crawler/test_sources.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 from src.pynavy.crawler.sources import *
+from src.pynavy.utility import directories
 
 
 class Test_Sources_Methods(unittest.TestCase):
@@ -9,11 +10,19 @@ class Test_Sources_Methods(unittest.TestCase):
         self.file_url = "https://www.example/file.pdf"
         self.local_file_path = __file__
         self.local_folder = os.path.join("path", "folder", "item")
+        self.folder_path = "folder"
+        directories.create_dir(self.folder_path)
 
+    def tearDown(self):
+        directories.delete_dir(self.folder_path)
+        
     def test_is_url(self):
         self.assertTrue(is_url(self.url))
         self.assertTrue(is_url(self.file_url))
         self.assertFalse(is_url(self.local_file_path))
+
+    def test_get_url_path(self):
+        self.assertEqual(get_url_path(self.file_url), "/file.pdf")
 
     def test_is_local_file(self):
         self.assertTrue(is_local_file(self.local_file_path))
@@ -50,6 +59,38 @@ class Test_Sources_Methods(unittest.TestCase):
         self.assertTrue(is_text_file("file.html"))
         self.assertTrue(is_text_file("file.txt"))
         self.assertFalse(is_text_file("file.pdf"))
+
+    def test_is_plain_text_file(self):
+        self.assertFalse(is_plain_text_file("file.html"))
+        self.assertTrue(is_plain_text_file("file.txt"))
+
+    def test_get_urls_from_html(self):
+        html = '''<div>
+        <a href='example.com'></a>
+        <a href='google.com'></a>
+        </div>'''
+        self.assertEqual(get_urls_from_html(html), {"example.com",
+        "google.com"})
+
+    def test_get_urls_from_text(self):
+        text = '''https://example.com/file.txt
+        http://example.com/
+        /folder/file.txt'''
+        self.assertEqual(get_urls_from_text(text), {
+            "https://example.com/file.txt",
+            "http://example.com/"
+        })
+
+    def test_get_file_paths(self):
+        file_path = os.path.join(self.folder_path, "file.txt")
+        file2_path = os.path.join(self.folder_path, "file.htm")
+        # creates files
+        directories.create_file(file_path)
+        directories.create_file(file2_path)
+        # checks if path to files is returned
+        self.assertCountEqual(get_file_paths(self.folder_path), {
+            file_path, file2_path
+        })
 
 
 if __name__ == '__main__':
