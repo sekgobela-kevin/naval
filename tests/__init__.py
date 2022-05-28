dbe6cb0a6324c1763c4a4b1646999bf9b1ccf59f
Sekgobela-Kevin <kevinnoko23@gmail.com>
2022-05-28 05:49:34 +0200

    Added project samples/ folder to sys.path
    samples/ contains sample files to be used when testing.
    This is great for testing parse classes as there is no tests
    for parse classes. It is placed in project directory as
    the files will also be used for examples.
    
    Maybe its not right to place it on project directory.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,19 +3,28 @@ import os, sys
 def get_project_dir():
     '''Return project root directory path'''
     # slit name of module by path seperator
-    split_path =__file__.split(os.sep)
+    split_path = os.path.abspath(__file__).split(os.sep)
     # remove folder name and module name part
     split_path = split_path[0:len(split_path)-2]
     # join back the split portions to create folder path
     return os.sep.join(split_path) + os.sep
 
-def add_project_source_to_path():
-    '''Adds project source directory to path'''
-    project_dir = get_project_dir()
-    assert os.path.isdir(project_dir), project_dir
-    sys.path.append(os.path.join(project_dir, "source"))
+def add_to_path(directory):
+    '''Adds directory to sys path'''
+    assert os.path.isdir(directory), directory
+    sys.path.append(directory)
+    assert directory in  sys.path
+
+def add_project_folder_to_path(*args):
+    '''Adds directory in project to path'''
+    folder_path = os.path.join(get_project_dir(), *args)
+    add_to_path(folder_path)
+
 
 # adds project source dir to system path
 # tests need access to naval package
 # unittest seems not to provide that
-add_project_source_to_path()
+add_project_folder_to_path("source")
+add_project_folder_to_path("samples")
+add_project_folder_to_path("")
+
