2b0df90e4292133efa2ca66812c6987d7b7d4dfe
Sekgobela-Kevin <kevinnoko23@gmail.com>
2022-05-29 08:48:24 +0200

    Fixed Fetch class, updated tests
    Fetch class was not reacting to source_locates_data argument. Supporting
    the argumnet meant having to implement methods of Fetch_Base using Master_Parse class.
    Tests did not identify the issue bacause the tests did not test the argumnet.
    
    source_locates_data argument has complicated the project. But that would
    make it easy for users when using the high level api such as extract_text().
    There seems to be other issues with fetch subpackage and its classes.
    
    Tests for fetch classes needs to be rewritten like that of parse tests.
    This issue was identified while creating examples, but that has complicated
    Fetch class.
    
    Master_Fetch and Fetch class needs to be reworked.

--- source/naval/fetch/fetch.py
+++ source/naval/fetch/fetch.py
@@ -3,41 +3,98 @@ import io
 from ..fetch.fetch_base import Fetch_Base
 from .master_fetch import Master_Fetch
 
+
+
+def get_fetch_class(source, source_locates_data=True):
+    fetch_class = Master_Fetch.get_fetch_class(
+        source,
+        source_locates_data=source_locates_data
+    )
+    return fetch_class
+
 class Fetch(Fetch_Base):
     '''Main class for fetching data from source'''
-    def __init__(self, source, content_type=None, source_locates_data=True):
+    def __init__(self, source, source_locates_data=True, content_type=None):
+        '''source - url, file path or gile object, etc\n
+        source_locates_data(optional) - true if source is resource locator 
+        e.g url, file path, default True.\n
+        content_type(optional) - content type of data to be fetched. E.g 
+        html, .html or text/html. Default is None'''
         # create fetch object for source
-        self.fetch_obj = Master_Fetch.get_fetch_object(source, source_locates_data)
-        super().__init__(source, content_type)
+        self.fetch_class = get_fetch_class(source, source_locates_data)
+
+        # this was taken from Fetch_Base __init__()
+        # super().__init__() would raise errors sometimes
+        # it doesnt support source_locates_data
+        self._source = source
+        self._source_text = self.source_to_text(source, 
+        source_locates_data=source_locates_data)
+
+        assert isinstance(self._source_text, str), self._source_text
+        self.content_type = self.guess_type(source, 
+        source_locates_data = source_locates_data)
+        
+        self.file = self.open(source, source_locates_data=source_locates_data)
+
+    @classmethod
+    def open(cls, source: str, source_locates_data=True, 
+    *args, **kwargs) -> io.IOBase:
+        '''Opens file file for storing fetched data\n
+        source - url, filepath, file object, etc\n
+        *args, **kwagrs - optional arguments to pass to open() function\n'''
+        # files should always be opened in binary mode
+        fetch_class = get_fetch_class(source, source_locates_data)
+        return fetch_class.open(source, *args, **kwargs)
+
     
     @classmethod
-    def source_to_text(cls, source) -> str:
+    def source_to_text(cls, source, source_locates_data=True) -> str:
         '''Returns text version of source. e.g file object would
         return its path or file name'''
-        return Master_Fetch.source_to_text(source)
+        fetch_class = get_fetch_class(source, source_locates_data)
+        return fetch_class.source_to_text(source)
+
 
     @classmethod
-    def is_source_valid(cls, source: str) -> bool:
+    def source_to_content_type(cls, source, source_locates_data=True):
+        '''Return content type of source(url, filepath, etc)\n
+        source - url, filepath, file object, etc\n'''
+        fetch_class = get_fetch_class(source, source_locates_data)
+        return fetch_class.source_to_content_type(source)
+
+
+    @classmethod
+    def guess_type(cls, source, content_type=None, source_locates_data=True):
+        '''Guess content type from text source and another content type\n
+        source - url, filepath, file object, etc\n
+        content_type - string in form e.g 'text/', '.html', 
+        'html', 'text/plain' or 'file.txt'''
+        fetch_class = get_fetch_class(source, source_locates_data)
+        return fetch_class.guess_type(source)
+
+
+    @classmethod
+    def is_source_valid(cls, source, source_locates_data=True) -> bool:
         '''Checks if source is valid\n
         source - url, file path, file object, etc'''
-        return Master_Fetch.is_source_valid(source)
+        return Master_Fetch.is_source_valid(source, source_locates_data)
 
     @classmethod
-    def is_source_active(cls, source: str) -> bool:
+    def is_source_active(cls, source, source_locates_data=True) -> bool:
         '''Checks if source is active\n
         source - url, file path, file object, etc'''
-        return Master_Fetch.is_source_active(source)
+        return Master_Fetch.is_source_active(source, source_locates_data)
 
     @classmethod
-    def fetch_to_file(cls, source: str, file: io.FileIO) -> str:
-        '''Read raw data from source and save to file\n
-        source - url, file path, file object, etc\n
-        file - file like object to store data'''
-        return Master_Fetch.fetch_to_file(source, file)
+    def fetch_to_file(cls, source, file: io.IOBase, source_locates_data=True):
+        '''Fetch data from source to file and return file object\n
+        source - url, filepath, file object, etc'''
+        fetch_class = get_fetch_class(source, source_locates_data)
+        return fetch_class.fetch_to_file(source, file)
 
-    def get_fetch(self):
-        '''Returns underlying fetch object'''
-        return self.fetch_obj
+    def get_fetch_class(self):
+        '''Returns underlying fetch class of the object'''
+        return self.fetch_class
 
 if __name__ == "__main__":
     fetch_obj = Fetch("https://www.tutorialspoint.com/")
--- tests/unit/fetch/test_fetch.py
+++ tests/unit/fetch/test_fetch.py
@@ -3,6 +3,7 @@ import unittest
 import tempfile
 from naval.fetch.fetch import Fetch
 from naval.fetch.fetch_base import Fetch_Base
+from naval.fetch.file_fetch import File_Fetch
 
 
 class Test_Fetch(unittest.TestCase):
@@ -13,6 +14,8 @@ class Test_Fetch(unittest.TestCase):
         self.file_object2 = open(self.file_path)
         self.fetch_obj = Fetch(self.file_object)
 
+        self.html = "<div> division </div>"
+
     def tearDown(self):
         self.file_object.close()
         self.file_object2.close()
@@ -21,30 +24,40 @@ class Test_Fetch(unittest.TestCase):
     def test_source_to_text(self):
         self.assertEqual(Fetch.source_to_text(self.file_path), 
         self.file_path)
+        with self.assertRaises(Exception):
+            Fetch.source_to_text(self.html)
+        Fetch.source_to_text(self.html, source_locates_data=False)
 
     def test_open(self):
         file_obj = self.fetch_obj.open(self.file_object)
-        self.assertIsInstance(file_obj, IOBase)
+        self.assertEqual(file_obj, self.file_object)
         file_obj.close()
 
     def test_is_source_valid(self):
         # is_source_valid() only checks if source is file
         self.assertTrue(Fetch.is_source_valid(self.file_object))
         self.assertTrue(Fetch.is_source_valid(__file__))
+        self.assertFalse(Fetch.is_source_valid(self.html))
+        valid = Fetch.is_source_valid(self.html, source_locates_data=False)
+        self.assertTrue(valid)
+
 
     def test_is_source_active(self):
         # file object is already active
         self.assertTrue(Fetch.is_source_active(self.file_object))
         # this file doesnt exists(inactive)
         self.assertFalse(Fetch.is_source_active("not_exists.file"))
+        self.assertFalse(Fetch.is_source_active(self.html))
+        valid = Fetch.is_source_active(self.html, source_locates_data=False)
+        self.assertTrue(valid)
 
     def test_fetch_to_file(self):
         self.fetch_obj.fetch_to_file(self.file_path, self.file_object)
         self.assertGreater(self.file_object.tell(), 0)
 
-    def test_get_fetch(self):
-        fetch_obj = self.fetch_obj.get_fetch()
-        self.assertIsInstance(fetch_obj, Fetch_Base)
+    def test_get_fetch_class(self):
+        fetch_class = self.fetch_obj.get_fetch_class()
+        self.assertEqual(fetch_class, File_Fetch)
 
 if __name__ == '__main__':
     unittest.main()
